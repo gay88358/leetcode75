a2b2fc9ffb28e210d7734e3ce0eaf22d4547349d
mars <mars.hong@sunbirddcim.com>
2022-02-14 10:31:20 +0800

    change test format of leetcode55.

--- src/test/java/leetcode55/SolutionTest.java
+++ src/test/java/leetcode55/SolutionTest.java
@@ -2,30 +2,31 @@ package leetcode55;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class SolutionTest {
     @Test
     void jump_from_one_value_array() {
         var s = new Solution();
 
-        assertTrue(s.canJump(new int[] { 1 }));
-        assertTrue(s.canJump(new int[] { 0 }));
+        assertTrue(s.canJump(new int[]{ 1 }));
+        assertTrue(s.canJump(new int[]{ 0 }));
     }
 
     @Test
     void jump_from_two_value_array() {
         var s = new Solution();
 
-        assertTrue(s.canJump(new int[] { 1, 1 }));
-        assertTrue(s.canJump(new int[] { 0 }));
+        assertTrue(s.canJump(new int[]{ 1, 1 }));
+        assertTrue(s.canJump(new int[]{ 0 }));
     }
 
     @Test
     void jump_from_five_value_array() {
         var s = new Solution();
 
-        assertTrue(s.canJump(new int[] { 2,3,1,1,4 }));
-        assertFalse(s.canJump(new int[] { 3,2,1,0,4 }));
+        assertTrue(s.canJump(new int[]{ 2, 3, 1, 1, 4 }));
+        assertFalse(s.canJump(new int[]{ 3, 2, 1, 0, 4 }));
     }
 }
